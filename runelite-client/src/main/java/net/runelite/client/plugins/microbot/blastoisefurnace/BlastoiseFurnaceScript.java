5478e7ffdbffdb9d980e091446432bbbf186928d
Tim <timothy.c0001@gmail.com>
2025-04-07 20:44:05 -0700

    Added timing delay

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/blastoisefurnace/BlastoiseFurnaceScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/blastoisefurnace/BlastoiseFurnaceScript.java
@@ -63,8 +63,8 @@ public class BlastoiseFurnaceScript extends Script {
         state = State.BANKING;
         primaryOreEmpty = !Rs2Inventory.hasItem(config.getBars().getPrimaryOre());
         secondaryOreEmpty = !Rs2Inventory.hasItem(config.getBars().getSecondaryOre());
-        Rs2Antiban.resetAntibanSettings();
-        applyAntiBanSettings();
+        //Rs2Antiban.resetAntibanSettings();
+        //applyAntiBanSettings();
 
         this.mainScheduledFuture = this.scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
@@ -83,6 +83,7 @@ public class BlastoiseFurnaceScript extends Script {
                         Microbot.status = "Banking";
                         if (!Rs2Bank.isOpen()) {
                             System.out.println("Opening bank");
+                            sleep(1250,2000);
                             Rs2Bank.openBank();
                             sleepUntil(Rs2Bank::isOpen, 20000);
                         }
@@ -120,7 +121,7 @@ public class BlastoiseFurnaceScript extends Script {
                             this.shutdown();
                         }
 
-                        if (!Rs2Player.hasStaminaBuffActive() && Microbot.getClient().getEnergy() < 8100) {
+                        if (!Rs2Player.hasStaminaBuffActive() && Microbot.getClient().getEnergy() < 7500) {
                             this.useStaminaPotions();
                         }
 
@@ -184,8 +185,10 @@ public class BlastoiseFurnaceScript extends Script {
 
             if (super.run()) {
                 if (canLootBar) {
+                    sleep(1000,1500);
                     Rs2Keyboard.keyPress(KeyEvent.VK_SPACE);
                 } else if (multipleBarTypes) {
+                    sleep(1000,1500);
                     Rs2Keyboard.keyPress(KeyEvent.VK_SPACE);
                 }
                 Rs2Inventory.waitForInventoryChanges(5000);
@@ -243,9 +246,10 @@ public class BlastoiseFurnaceScript extends Script {
             Rs2Bank.withdrawAll(GOLD_ORE);
             return;
         }
-//        Rs2Bank.closeBank();
-        sleep(500,1000);
+        Rs2Bank.closeBank();
+        sleep(750,1250);
         depositOre();
+        sleep(750,1250);
 
         if (Rs2Random.dicePercentage(50)) { // 50% chance
             Rs2Walker.walkFastCanvas(new WorldPoint(1940, 4962, 0));
@@ -525,6 +529,7 @@ public class BlastoiseFurnaceScript extends Script {
 
     private void equipGoldSmithGauntlets() {
         if (config.getBars().isRequiresGoldsmithGloves()) {
+            sleep(1500,2500);
             Rs2Inventory.interact(ItemID.GOLDSMITH_GAUNTLETS, "Wear");
         }
     }
