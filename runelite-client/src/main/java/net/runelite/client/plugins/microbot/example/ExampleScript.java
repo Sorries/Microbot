5c4fe7188c5dfa290f0e933783b6ae86599241d8
Tim <timothy.c0001@gmail.com>
2025-06-02 07:14:24 -0700

    - temp

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -1,8 +1,57 @@
 package net.runelite.client.plugins.microbot.example;
 
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
+import net.runelite.client.plugins.microbot.util.camera.Rs2Camera;
+import net.runelite.client.plugins.microbot.util.combat.Rs2Combat;
+import net.runelite.client.plugins.microbot.util.dialogues.Rs2Dialogue;
+import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
+import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
+import net.runelite.client.plugins.microbot.util.grounditem.Rs2GroundItem;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
+import net.runelite.client.plugins.microbot.util.keyboard.Rs2Keyboard;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
+import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
+import net.runelite.client.plugins.microbot.util.npc.Rs2NpcModel;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.player.Rs2PlayerModel;
+import net.runelite.client.plugins.microbot.util.security.Login;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
+import lombok.SneakyThrows;
+import net.runelite.api.*;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2AntibanSettings;
+import net.runelite.client.plugins.microbot.util.bank.enums.BankLocation;
+import net.runelite.client.plugins.microbot.util.camera.Rs2Camera;
+import net.runelite.client.plugins.microbot.util.coords.Rs2LocalPoint;
+import net.runelite.client.plugins.microbot.util.coords.Rs2WorldArea;
+import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
+import net.runelite.client.plugins.microbot.util.menu.NewMenuEntry;
+import net.runelite.client.plugins.microbot.util.misc.Rs2UiHelper;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.tile.Rs2Tile;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
+import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.runelite.client.plugins.microbot.util.Global.sleepUntil;
+
+
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ExampleScript extends Script {
@@ -18,6 +67,15 @@ public class ExampleScript extends Script {
                 long startTime = System.currentTimeMillis();
 
                 // CODE HERE
+//                int distance = Rs2Player.getWorldLocation().distanceTo(Objects.requireNonNull(Rs2Npc.getNpcs(2050).findFirst().orElse(null)).getWorldLocation());
+//                        System.out.println(distance);
+
+ //               Rs2GameObject.interact(new WorldPoint(3762,3755,0),"Build");
+ //               Rs2GameObject.interact(30568,"Build");
+                TileObject t = Rs2GameObject.findObjectByLocation(new WorldPoint(3677, 3882, 0));
+                System.out.println(t);
+                System.out.println(t.getId());
+//                Rs2GameObject.interact(new WorldPoint(3768, 3761, 0),"Build");
 
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
