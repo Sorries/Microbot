5fb3583b75462d7a2fcb5d484c015ae21cf40612
Tim <timothy.c0001@gmail.com>
2025-06-22 22:56:02 -0700

    - Check for gameObject call Altar

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -1,9 +1,11 @@
 package net.runelite.client.plugins.microbot.example;
 
+import net.runelite.api.GameObject;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.plugindisabler.PluginDisablerPlugin;
 import net.runelite.client.plugins.microbot.storm.plugins.PlayerMonitor.PlayerMonitorPlugin;
+import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.qualityoflife.scripts.SmartSlayer;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2ItemModel;
@@ -33,22 +35,22 @@ public class ExampleScript extends Script {
 //                }else{
 //                    Microbot.log("Slayer task is not complete!");
 //                }
-                for (Rs2ItemModel item : Rs2Inventory.items().stream()
-                        .filter(x -> {
-                            String[] actions = x.getInventoryActions();
-                            return actions != null && (Arrays.asList(actions).contains("Wield") || Arrays.asList(actions).contains("Wear"));
-                        })
-                        .collect(Collectors.toList())) {
-
-                    if (Arrays.asList(item.getInventoryActions()).contains("Wield")) {
-                        Rs2Inventory.interact(item, "Wield");
-                    } else if (Arrays.asList(item.getInventoryActions()).contains("Wear")) {
-                        Rs2Inventory.interact(item, "Wear");
-                    }
-
-                    // Debug (optional)
-                    System.out.println("Equipped: " + item.getName()+ " Item Actions: " + Arrays.toString(item.getInventoryActions()));
-                }
+//                for (Rs2ItemModel item : Rs2Inventory.items().stream()
+//                        .filter(x -> {
+//                            String[] actions = x.getInventoryActions();
+//                            return actions != null && (Arrays.asList(actions).contains("Wield") || Arrays.asList(actions).contains("Wear"));
+//                        })
+//                        .collect(Collectors.toList())) {
+//
+//                    if (Arrays.asList(item.getInventoryActions()).contains("Wield")) {
+//                        Rs2Inventory.interact(item, "Wield");
+//                    } else if (Arrays.asList(item.getInventoryActions()).contains("Wear")) {
+//                        Rs2Inventory.interact(item, "Wear");
+//                    }
+//
+//                    // Debug (optional)
+//                    System.out.println("Equipped: " + item.getName()+ " Item Actions: " + Arrays.toString(item.getInventoryActions()));
+//                }
 
 //                for (Rs2ItemModel item : Rs2Inventory.items().stream()
 //                        .filter(x -> Arrays.asList(x.getInventoryActions()).contains("Wear"))
@@ -58,6 +60,8 @@ public class ExampleScript extends Script {
 //                    // System.out.println("Wearing: " + item.getName());
 //                }
 
+                System.out.println("Object"+ Rs2GameObject.getAll());
+                Rs2GameObject.getGameObject("Altar",true).getId();
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
                 System.out.println("Total time for loop " + totalTime);
