cdd6e2d00f95a9dc08528c5101fbea0030ec7698
Tim <timothy.c0001@gmail.com>
2025-07-09 19:05:33 -0700

    - rebase own script ontop

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/zerozero/birdhunter/BirdHunterScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/zerozero/birdhunter/BirdHunterScript.java
@@ -62,7 +62,7 @@ public class BirdHunterScript extends Script {
         updateHuntingArea(config);
 
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
-            Rs2Antiban.resetAntibanSettings();
+            //Rs2antiban.resetantibansettings();
             Rs2Antiban.antibanSetupTemplates.applyHunterSetup();
             Rs2AntibanSettings.actionCooldownChance = 0.1;
 
