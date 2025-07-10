4a6f88259a04fa8657781f83d1c59158974701a1
Tim <timothy.c0001@gmail.com>
2025-07-09 18:33:51 -0700

    - rebase own script ontop

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/farmTreeRun/FarmTreeRunScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/farmTreeRun/FarmTreeRunScript.java
@@ -21,7 +21,6 @@ import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
 import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
-import net.runelite.client.plugins.microbot.util.magic.Rs2Spellbook;
 import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
 import net.runelite.client.plugins.microbot.util.npc.Rs2NpcModel;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
@@ -94,7 +93,7 @@ public class FarmTreeRunScript extends Script {
 
     public boolean run(FarmTreeRunConfig config) {
         Microbot.enableAutoRunOn = false;
-        ////Rs2antiban.resetantibansettings();
+        //Rs2Antiban.resetAntibanSettings();
         Rs2AntibanSettings.naturalMouse = true;
         Rs2Antiban.setActivityIntensity(ActivityIntensity.LOW);
 
@@ -107,7 +106,7 @@ public class FarmTreeRunScript extends Script {
 
                 long startTime = System.currentTimeMillis();
                 if (Rs2AntibanSettings.actionCooldownActive) return;
-                if(!Rs2Magic.isSpellbook(Rs2Spellbook.MODERN)){
+                if(!Rs2Magic.isModern()){
                     log("Not on modern spellbook");
                     shutdown();
                 }
