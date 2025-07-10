4c1a9ac0e1a5aacb63382120bba3c8ba78002071
Tim <timothy.c0001@gmail.com>
2025-07-09 19:05:33 -0700

    - rebase own script ontop

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/magic/aiomagic/scripts/TeleAlchScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/magic/aiomagic/scripts/TeleAlchScript.java
@@ -29,7 +29,7 @@ public class TeleAlchScript extends Script {
     public boolean run() {
         Microbot.enableAutoRunOn = false;
         //Rs2antiban.resetantibansettings();
-        //Rs2Antiban.antibanSetupTemplates.applyGeneralBasicSetup();
+        Rs2Antiban.antibanSetupTemplates.applyGeneralBasicSetup();
         Rs2AntibanSettings.simulateAttentionSpan = true;
         Rs2AntibanSettings.nonLinearIntervals = true;
         Rs2AntibanSettings.contextualVariability = true;
