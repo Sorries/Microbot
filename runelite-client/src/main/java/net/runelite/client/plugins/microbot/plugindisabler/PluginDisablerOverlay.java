1aec00e76c173c96da9ee5a878c509eb2d29a21a
Tim <timothy.c0001@gmail.com>
2025-07-19 19:56:51 -0700

    - Added overlay for teleport out

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/plugindisabler/PluginDisablerOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/plugindisabler/PluginDisablerOverlay.java
@@ -82,8 +82,15 @@ public class PluginDisablerOverlay extends OverlayPanel {
                         .build());
             }
 
-            if (PluginDisablerScript.disablePluginsFlag){panelComponent.getChildren().add(LineComponent.builder().build());
-            panelComponent.getChildren().add(TitleComponent.builder()
+            if(config.teleOut()){
+                panelComponent.getChildren().add(TitleComponent.builder()
+                        .text("Will teleport Out")
+                        .color(Color.BLUE)
+                        .build());
+            }
+
+            if (PluginDisablerScript.disablePluginsFlag){
+                panelComponent.getChildren().add(TitleComponent.builder()
                     .text("Enabled")
                     .color(Color.GREEN)
                     .build());
