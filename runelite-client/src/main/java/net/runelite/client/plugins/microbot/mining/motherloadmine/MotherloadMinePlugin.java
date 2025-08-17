4f9c2e766e2b873cb8a25840cf4f6f09bba5d223
Tim <timothy.c0001@gmail.com>
2025-08-17 15:49:28 -0700

    - added back config to work

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/MotherloadMinePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/MotherloadMinePlugin.java
@@ -51,7 +51,7 @@ public class MotherloadMinePlugin extends Plugin {
     @Override
     protected void startUp() throws AWTException {
         overlayManager.add(motherloadMineOverlay);
-        motherloadMineScript.run();
+        motherloadMineScript.run(config);
     }
 
     @Subscribe
