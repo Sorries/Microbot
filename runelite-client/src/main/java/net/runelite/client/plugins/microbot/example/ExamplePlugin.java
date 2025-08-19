7cfedb514cbdaa1f254ed7beb2ade3a432942bc1
Tim <N/A>
2025-08-19 12:05:59 -0700

    - added back deleted files

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
@@ -1,8 +1,10 @@
 package net.runelite.client.plugins.microbot.example;
 
+import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -11,35 +13,55 @@ import javax.inject.Inject;
 import java.awt.*;
 
 @PluginDescriptor(
-        name = PluginDescriptor.Default + "Example Plugin",
-        description = "Performance test for GameObject composition retrieval",
-        tags = {"performance", "microbot", "test", "gameobject"},
+        name = PluginDescriptor.Default + "Example",
+        description = "Microbot example plugin",
+        tags = {"example", "microbot"},
         enabledByDefault = false
 )
 @Slf4j
 public class ExamplePlugin extends Plugin {
     @Inject
+    private ExampleConfig config;
+    @Provides
+    ExampleConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(ExampleConfig.class);
+    }
+
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private ExampleOverlay exampleOverlay;
+
+    @Inject
     ExampleScript exampleScript;
-    @Inject
-    ExampleScriptOverlay exampleScriptOverlay;
-    @Inject
-    OverlayManager overlayManager;
 
 
     @Override
     protected void startUp() throws AWTException {
-        overlayManager.add(exampleScriptOverlay);
-        exampleScript.run();
+        if (overlayManager != null) {
+            overlayManager.add(exampleOverlay);
+            exampleOverlay.myButton.hookMouseListener();
+        }
+        exampleScript.run(config);
     }
 
-    @Override
     protected void shutDown() {
-        overlayManager.remove(exampleScriptOverlay);
         exampleScript.shutdown();
+        overlayManager.remove(exampleOverlay);
+        exampleOverlay.myButton.unhookMouseListener();
+    }
+    int ticks = 10;
+    @Subscribe
+    public void onGameTick(GameTick tick)
+    {
+        //System.out.println(getName().chars().mapToObj(i -> (char)(i + 3)).map(String::valueOf).collect(Collectors.joining()));
+
+        if (ticks > 0) {
+            ticks--;
+        } else {
+            ticks = 10;
+        }
+
     }
 
-    // on settings change
-    @Subscribe
-    public void onConfigChanged(final ConfigChanged event) {
-    }
 }
