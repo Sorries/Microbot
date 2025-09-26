b5673d6d63ddb6a366e38f858666a9b912da02fe
Tim <timothy.c0001@gmail.com>
2025-09-26 13:24:11 -0700

    - Qol - initially added npcAggressionReset

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/NpcAggressionReset.java
@@ -0,0 +1,84 @@
+package net.runelite.client.plugins.microbot.qualityoflife.scripts;
+
+import com.google.inject.Inject;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.MicrobotPlugin;
+import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.qualityoflife.QoLConfig;
+import net.runelite.client.plugins.microbot.shortestpath.ShortestPathPlugin;
+import net.runelite.client.plugins.microbot.util.coords.Rs2WorldArea;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
+import net.runelite.client.plugins.npcunaggroarea.NpcAggroAreaPlugin;
+
+import java.awt.*;
+import java.awt.geom.Area;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+
+
+public class NpcAggressionReset extends Script {
+
+    private static final int SAFE_AREA_RADIUS = 10;
+    private final WorldPoint[] safeCenters = new WorldPoint[2];
+
+    public boolean run(QoLConfig config) {
+        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            try {
+                if (!Microbot.isLoggedIn()) return;
+                if (!super.run()) return;
+                if (!config.npcAggressionReset()) return;
+                if (Microbot.isPluginEnabled(NpcAggroAreaPlugin.class)){
+                    NpcAggroAreaPlugin plugin = Microbot.getPlugin(NpcAggroAreaPlugin.class);
+                    if (plugin != null) {
+                        this.safeCenters[0] = plugin.getSafeCenters()[0];
+                        this.safeCenters[1] = plugin.getSafeCenters()[1];
+                        Microbot.log("Npc safe wp: " + Arrays.toString(this.safeCenters));
+                        Microbot.log("Aggression Timer: " + Duration.between(Instant.now(),plugin.getEndTime()).toSeconds());
+                        Microbot.log("Generated Area: "+ generateSafeArea().toString());
+                        if (Duration.between(Instant.now(),plugin.getEndTime()).toSeconds() <= 0 ){
+
+                        }
+
+
+                    } else {
+                        Microbot.log("Npc Agro is not running.");
+                    }
+
+                }
+
+            } catch(Exception ex) {
+                Microbot.logStackTrace(this.getClass().getSimpleName(), ex);
+            }
+        }, 0, 1000, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    private Area generateSafeArea()
+    {
+        final Area area = new Area();
+
+        for (WorldPoint wp : safeCenters)
+        {
+            if (wp == null)
+            {
+                continue;
+            }
+
+            Polygon poly = new Polygon();
+            poly.addPoint(wp.getX() - SAFE_AREA_RADIUS, wp.getY() - SAFE_AREA_RADIUS);
+            poly.addPoint(wp.getX() - SAFE_AREA_RADIUS, wp.getY() + SAFE_AREA_RADIUS + 1);
+            poly.addPoint(wp.getX() + SAFE_AREA_RADIUS + 1, wp.getY() + SAFE_AREA_RADIUS + 1);
+            poly.addPoint(wp.getX() + SAFE_AREA_RADIUS + 1, wp.getY() - SAFE_AREA_RADIUS);
+            area.add(new Area(poly));
+        }
+
+        return area;
+    }
+
+}
+
