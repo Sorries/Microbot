cb3393498b9127703cd929d80736dbab5ca5f962
Tim <N/A>
2026-02-20 18:38:59 -0800

    - Rs2Walker - Added back close minimap before walking

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/Rs2Walker.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/Rs2Walker.java
@@ -998,7 +998,6 @@ public class Rs2Walker {
         if (reachableTileCheck || (!walkableCheck && distToTarget <= distance)) {
             return WalkerState.ARRIVED;
         }
-
         final Pathfinder pathfinder = ShortestPathPlugin.getPathfinder();
         if (pathfinder != null && !pathfinder.isDone()) {
             return WalkerState.MOVING;
