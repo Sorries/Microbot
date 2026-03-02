db253c1768bfc614210eaa79413e81e96fdcdd5d
Tim <timothy.c0001@gmail.com>
2026-03-02 13:50:58 -0800

    - MicrobotPlugin - Added back ItemId when menuEntry Click - Rs2Walker - Added a block list for doors within a region or black listed name

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/Rs2Walker.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/Rs2Walker.java
@@ -1090,11 +1090,11 @@ public class Rs2Walker {
                 boolean adjacentToPath = probe.distanceTo(fromWp) <= 1 || probe.distanceTo(toWp) <= 1;
                 if (!adjacentToPath || !Objects.equals(probe.getPlane(), Microbot.getClient().getLocalPlayer().getWorldLocation().getPlane())) continue;
 
-                WallObject wall = Rs2GameObject.getWallObject(o -> o.getWorldLocation().equals(probe), probe, 3);
+                WallObject wall = Rs2GameObject.getWallObject(o -> o.getWorldLocation().equals(probe), probe, 1);
 
                 TileObject object = (wall != null)
                         ? wall
-                        : Rs2GameObject.getGameObject(o -> o.getWorldLocation().equals(probe), probe, 3);
+                        : Rs2GameObject.getGameObject(o -> o.getWorldLocation().equals(probe), probe, 1);
                 if (object == null) continue;
 
                 ObjectComposition comp = Rs2GameObject.convertToObjectComposition(object);
@@ -1128,7 +1128,25 @@ public class Rs2Walker {
                 }
 
                 if (found) {
+                    Set<Integer> BLOCKED_REGIONS = Set.of(
+                            13150
+                    );
+                    if (BLOCKED_REGIONS.contains(Rs2Player.getWorldLocation().getRegionID())) {
+                        Microbot.log("Skipped regions: " + Rs2Player.getWorldLocation().getRegionID() + ", Due to: " + name);
+                        continue;
+                    }
+
+                    List<String> BLOCKED_DOORS = List.of(
+                            "bamboo door"
+                    );
+                    String lowerName = name.toLowerCase();
+                    if (BLOCKED_DOORS.stream().anyMatch(lowerName::contains)) {
+                        Microbot.log("Skipped due to name: " + name);
+                        continue;
+                    }
+
                     if (!handleDoorException(object, action)) {
+                        Microbot.log("Door Object: " + object + ", Door action: " + action);
                         Rs2GameObject.interact(object, action);
                         Rs2Player.waitForWalking();
                     }
