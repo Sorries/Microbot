3d381c057fdb96d73211b8bcf99b63436a7475f2
Tim <timothy.c0001@gmail.com>
2026-03-15 23:58:28 -0700

    - HerbiboarScript - turn off item charge improved before depositing herb sack - HerbRun - added back

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/herbrun/HerbrunPlugin.java
@@ -0,0 +1,64 @@
+package net.runelite.client.plugins.microbot.herbrun;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.config.ConfigDescriptor;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+@Slf4j
+@PluginDescriptor(
+        name = PluginDescriptor.Mocrosoft + "Herb runner",
+        description = "Herb runner",
+        tags = {"herb", "farming", "money making", "skilling"},
+        enabledByDefault = false
+)
+public class HerbrunPlugin extends Plugin{
+    @Inject
+    private HerbrunConfig config;
+    @Provides
+    HerbrunConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(HerbrunConfig.class);
+    }
+
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private HerbrunOverlay HerbrunOverlay;
+
+    @Inject
+    HerbrunScript herbrunScript;
+
+    static String status;
+    
+
+    @Override
+    protected void startUp() throws AWTException {
+        if (overlayManager != null) {
+            overlayManager.add(HerbrunOverlay);
+        }
+        herbrunScript.run();
+    }
+
+    protected void shutDown() {
+        herbrunScript.shutdown();
+        overlayManager.remove(HerbrunOverlay);
+    }
+
+
+    public ConfigDescriptor getConfigDescriptor() {
+        if (Microbot.getConfigManager() == null) {
+            return null;
+        }
+        HerbrunConfig conf = Microbot.getConfigManager().getConfig(HerbrunConfig.class);
+        return Microbot.getConfigManager().getConfigDescriptor(conf);
+    }
+
+}
