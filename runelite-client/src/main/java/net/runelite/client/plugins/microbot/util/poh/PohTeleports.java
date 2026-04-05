4571cdfb6962b4feb63be2caca70469f5c6ac5a7
Tim <timothy.c0001@gmail.com>
2026-04-05 16:23:01 -0700

    - PoH - Update detection of exit portal

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/PohTeleports.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/PohTeleports.java
@@ -9,6 +9,9 @@ import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.ObjectID;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.api.tileobject.Rs2TileObjectCache;
+import net.runelite.client.plugins.microbot.api.tileobject.Rs2TileObjectQueryable;
+import net.runelite.client.plugins.microbot.api.tileobject.models.Rs2TileObjectModel;
 import net.runelite.client.plugins.microbot.shortestpath.Transport;
 import net.runelite.client.plugins.microbot.shortestpath.TransportType;
 import net.runelite.client.plugins.microbot.util.equipment.JewelleryLocationEnum;
@@ -45,14 +48,12 @@ public class PohTeleports {
      * @return
      */
     public static boolean isInHouse() {
-        if (!Rs2Player.IsInInstance()) return false;
-        // Use the tile-object cache rather than Rs2GameObject.getGameObject; the latter
-        // routes through Rs2Player.getWorldLocation() as a scene anchor which returns the
-        // overworld-template tile inside a POH instance and breaks the scene lookup.
-        return Microbot.getRs2TileObjectCache()
+        //return Rs2Player.IsInInstance() && Rs2GameObject.getGameObject(ObjectID.POH_EXIT_PORTAL) != null;
+        Rs2TileObjectModel exitPortal = Microbot.getRs2TileObjectCache()
                 .query()
                 .withId(ObjectID.POH_EXIT_PORTAL)
-                .nearest() != null;
+                .nearest();
+        return Rs2Player.IsInInstance() && exitPortal != null;
     }
 
     /**
