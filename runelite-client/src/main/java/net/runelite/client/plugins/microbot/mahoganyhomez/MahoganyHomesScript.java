c66196331f852bee71c8bbeec3f237274a0bb7bd
Tim <timothy.c0001@gmail.com>
2026-04-12 12:01:43 -0700

    - Rs2Walker - Revert retry (partialPath) - MahoganyHomes - Added a Integer.MAX_VALUE to use ladder

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/mahoganyhomez/MahoganyHomesScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/mahoganyhomez/MahoganyHomesScript.java
@@ -182,6 +182,11 @@ public class MahoganyHomesScript extends Script {
                 tryToUseLadder();
                 return;
             }
+            if (pathDistance == Integer.MAX_VALUE) {
+                log("Someone home, trying to use ladder.");
+                tryToUseLadder();
+                return;
+            }
             log("Local Path Distance is too far or unreachable, switching to WebWalker.");
 
             WalkerState state = Rs2Walker.walkWithState(object.getWorldLocation(), 3);
