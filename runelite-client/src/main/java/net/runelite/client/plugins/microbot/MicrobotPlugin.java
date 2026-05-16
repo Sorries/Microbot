6bbd3973479ba20243363e36f2d13528445319d4
Tim <timothy.c0001@gmail.com>
2026-05-16 08:40:37 -0700

    - Revised blockingEventManager

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/MicrobotPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/MicrobotPlugin.java
@@ -151,6 +151,7 @@ public class MicrobotPlugin extends Plugin
 		);
 
 		Microbot.pauseAllScripts.set(false);
+		Microbot.getBlockingEventManager().start();
 
 		MicrobotPluginListPanel pluginListPanel = pluginListPanelProvider.get();
 		pluginListPanel.addFakePlugin(new MicrobotPluginConfigurationDescriptor(
