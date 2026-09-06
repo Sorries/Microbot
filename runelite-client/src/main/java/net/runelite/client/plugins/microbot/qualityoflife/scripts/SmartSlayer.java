971c04d1b917f6f92eeaa3f34bf3c8aa17054252
Tim <timothy.c0001@gmail.com>
2026-09-06 10:18:06 -0700

    - Smart Slayer - Kraken Mode Revise v7.5

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/SmartSlayer.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/SmartSlayer.java
@@ -247,7 +247,7 @@ private static String slayerMonster = null;
                         else if (Rs2Inventory.itemQuantity("Fishing explosive") > 0) {
                             Rs2NpcModel whirlpool = Rs2Npc.getNpc(NpcID.SLAYER_KRAKEN_BOSS_WHIRLPOOL);
                             assert kraken != null;
-                            if (kraken.isDead()){
+                            if (!kraken.isDead()){
                                 return;
                             }
                             if (whirlpool != null) {
